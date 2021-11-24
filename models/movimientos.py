60ba0496b23b1f0ab95d7babb66ec2fe2166ec58
Squall-The-Crow <mzamora@sitecnet.com>
2021-11-24 03:35:46 -0600

    Update movimientos.py

--- models/movimientos.py
+++ models/movimientos.py
@@ -30,7 +30,7 @@ class movimientos(models.Model):
             vals = {
                 'name': line.name,
                 'estado': 'disponible',
-                #'producto': line.producto,
+                'producto': line.producto.id,
                 'documento': self.documento,
                 'movimiento_entrada': line.movimiento_id.id
             }        

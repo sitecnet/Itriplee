dd3686c25f3dec776bb6b0962d815851695bac51
Squall-The-Crow <mzamora@sitecnet.com>
2021-12-07 03:39:12 -0600

    Update servicio.py

--- models/servicio.py
+++ models/servicio.py
@@ -69,7 +69,8 @@ class servicio(models.Model):
     estado_refacciones = fields.Selection([
     	("disponible","Se pueden solicitar refacciones"),
     	("solicitadas","Refacciones ya solicitadas"),
-    	("liberadas","Las refacciones fueron liberadas")
+    	("surtida","Las refacciones fueron surtidas")
+        ("regresadas","Las refacciones fueron regresadas o vendidas")
         ], 'Estado de las refacciones del servicio', default='disponible')
     refacciones = fields.Many2one('itriplee.movimientos', 'Refacciones Solicitadas')
 

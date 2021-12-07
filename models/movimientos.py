ebcedf92cde94e632e2b3534aed4017c5a45516b
Squall-The-Crow <mzamora@sitecnet.com>
2021-12-07 13:40:45 -0600

    Update movimientos.py

--- models/movimientos.py
+++ models/movimientos.py
@@ -128,7 +128,7 @@ class SeriesWizard(models.TransientModel):
             })
             for prod in active_obj.productos:
                 if serie == prod.producto:
-                    prod.write(
+                    prod.update(
                     {'seriesdisponibles': line.seriesdisponibles})
             
 
